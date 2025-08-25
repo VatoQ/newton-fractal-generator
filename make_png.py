3be1379aa8b748d6b0551bee1a14bc5169cda063
Jonathan Enns <jonathanenns97@web.de>
2025-08-25 17:08:49 +0200

    log progress

--- make_png.py
+++ make_png.py
@@ -1,8 +1,10 @@
 import csv
 from PIL import Image
 
+print("Enter make_png.py")
 output_image = "fractal.png"
 
+print("\tRead data from csv...")
 pixel_list:list[tuple[int, int, int]] = []
 with open("image_data.csv", "r", newline="") as csv_file:
     reader = csv.reader(csv_file, delimiter=";")
@@ -15,9 +17,13 @@ with open("image_data.csv", "r", newline="") as csv_file:
     for row in reader:
         pixel_list.append((int(row[0]), int(row[1]), int(row[2])))
 
+print("\tMake new image...")
 
 image = Image.new("RGB", (X, Y))
 
+
 image.putdata(pixel_list)
 
+print("\tSave image...")
 image.save(output_image)
+print("Exit make_png")
